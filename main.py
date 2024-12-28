c9c5135c21ae22a023966331bb2b3c25366fefd3
Alexander Lee <alexanderlee@Alexanders-MacBook-Air.local>
2024-12-27 18:28:16 -0800

    changes

--- main.py
+++ main.py
@@ -13,10 +13,24 @@ orange = pygame.Color(255, 165, 0)
 turquoise = pygame.Color(48, 213, 200)
 Snake_speed = 10
 
-
+# Pygame initialization
 pygame.init()
 pygame.display.set_caption('Snakes')
 game_window = pygame.display.set_mode((window_x, window_y))
+fps = pygame.time.Clock()
+
+# Snake Initialization
+snake_head = [30, 480]
+snake_body = [[0, 480], [10, 480], [20, 480], [30, 480]]
+
+# Fruit
+random_fruit = [10* random.randint(1, 72), 10* random.randint(1, 32)]
+fruit_spawn = True
+
+direction = "RIGHT"
+change_to = direction
+
 
 while True:
     pygame.display.update()
+    break
